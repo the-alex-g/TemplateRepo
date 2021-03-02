8370d0a0912a312e521622c763decfcd3002722a
the-alex-g <62472057+the-alex-g@users.noreply.github.com>
2021-03-02 08:54:49 -0500

    Update default.gd

--- template_scripts/default.gd
+++ template_scripts/default.gd
@@ -1,1 +1,23 @@
+ extends %BASE%
+
+# signals
+
+# enums
+
+# consts
+
+# exported variables
+
+# variables
+var _ignore
+
+# onready variables
+
+
+func _ready()->void%VOID_RETURN%:
+  pass
+
+
+func _process()->void%VOID_RETURN%:
+  pass
 
