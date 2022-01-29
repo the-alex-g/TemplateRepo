44471fffc7a56dc0469fd5e31274f7394572bd18
the-alex-g <62472057+the-alex-g@users.noreply.github.com>
2022-01-29 07:41:39 -0500

    Update draw.gd
    So it reflects the current StyleGuide recommendations

--- template_scripts/draw.gd
+++ template_scripts/draw.gd
@@ -1,20 +1,28 @@
 extends %BASE%
+# class description (one line)
+# extended class description
 
 # signals
 
-# constants
-
 # enums
+
+# constants
 
 # exported variables
 
-# normal variables
-var color := Color.white
+# public variables
+
+# private variables
+var _color := Color.white
 var _ignore
 
 # onready variables
 onready var collision := $CollisionShape2D
 
+# METHOD ORDER:
+# 1. built-in methods, starting with _init and _ready in that order
+# 2. public methods
+# 3. private methods
 
 func _ready()->void%VOID_RETURN%:
 	pass
@@ -29,18 +37,18 @@ func _draw()->void:
 		var shape = collision.get_shape()
 		if shape is CircleShape2D:
 			var radius = shape.radius
-			draw_circle(-Vector2(radius,radius), radius, color)
+			draw_circle(-Vector2(radius,radius), radius, _color)
 		elif shape is RectangleShape2D:
 			var extents : Vector2 = shape.extents
-			draw_rect(Rect2(-extents, extents*2), color)
+			draw_rect(Rect2(-extents, extents*2), _color)
 		elif shape is CapsuleShape2D:
 		var radius = shape.radius
 		var height = shape.height
 		if collision.rotation_degrees != 270 and collision.rotation_degrees != 90:
-			draw_circle(Vector2(0,height/2), radius, color)
-			draw_circle(-Vector2(0,height/2), radius, color)
-			draw_rect(Rect2(-Vector2(radius*2, height)/2, Vector2(radius*2, height)), color)
+			draw_circle(Vector2(0,height/2), radius, _color)
+			draw_circle(-Vector2(0,height/2), radius, _color)
+			draw_rect(Rect2(-Vector2(radius*2, height)/2, Vector2(radius*2, height)), _color)
 		else:
-			 draw_circle(Vector2(height/2,0), radius, color)
-			 draw_circle(-Vector2(height/2,0), radius, color)
-			 draw_rect(Rect2(-Vector2(height, radius*2)/2, Vector2(height, radius*2)), color)
+			 draw_circle(Vector2(height/2,0), radius, _color)
+			 draw_circle(-Vector2(height/2,0), radius, _color)
+			 draw_rect(Rect2(-Vector2(height, radius*2)/2, Vector2(height, radius*2)), _color)
